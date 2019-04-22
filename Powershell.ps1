0e93bbc16d3824fb7c3830342a39784e9c26542d
jayasimhamadhira <46185623+jayasimhamadhira@users.noreply.github.com>
2019-04-22 16:59:50 +0530

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,14 +1,4 @@
-    [string]$ServerName="sqlcredscan.database.windows.net"
-    [string]$UserName="jayasimha"
-    [string]$Password="Admin@12345678"
-    $DbName="sqlcredscan"
-    $RgName="Credscan-TFSV-RG"  
-    $RgLocation="Central US"
-    $storageaccountName="credscanrepo"
-    $ContainerName="credscan-container"
-    $tfvcRepositoryPath = "C:\TFVCRepopath"
-
-    $Armexist=Get-Module -Name AzureRM.* -ListAvailable
+$Armexist=Get-Module -Name AzureRM.* -ListAvailable
     if(!($Armexist.Count -gt 0))
     {
         Install-Module -Name AzureRm -AllowClobber -Force -Verbose
@@ -17,6 +7,85 @@
     $EP="P@sw0rd!13"
     $Subscription="c1bd9039-9169-41b6-9b75-6eef04aaf8a4"
     $AzureSubscriptionTenantId="72f988bf-86f1-41af-91ab-2d7cd011db47"
+    $azureAccountName = $EU
+    $azurePassword = ConvertTo-SecureString $EP -AsPlainText -Force
+    $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
+    Start-Sleep -Seconds 2
+    Write-Host $Subscription $AzureSubscriptionTenantId
+    $login = Add-AzureRmAccount -SubscriptionName $Subscription -TenantId $AzureSubscriptionTenantId -Credential $psCred 
+    if (!$login)
+    { 
+           return
+       } 
+    $login
+    Write-output "login completed" 
+         
+    
+    #Cypher Technique Function to to encrypted and Decrypt the value from Azure Key Vault
+#BEGIN
+function Set-Key {
+param([string]$string)
+$length = $string.length
+$pad = 32-$length
+if (($length -lt 16) -or ($length -gt 32)) {Throw "String must be between 16 and 32 characters"}
+$encoding = New-Object System.Text.ASCIIEncoding
+$bytes = $encoding.GetBytes($string + "0" * $pad)
+return $bytes
+}
+function Set-EncryptedData {
+param($key,[string]$plainText)
+$securestring = new-object System.Security.SecureString
+$chars = $plainText.toCharArray()
+foreach ($char in $chars) {$secureString.AppendChar($char)}
+$encryptedData = ConvertFrom-SecureString -SecureString $secureString -Key $key
+return $encryptedData
+}
+function Get-EncryptedData {
+param($key,$data)
+$data | ConvertTo-SecureString -key $key |
+ForEach-Object {[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($_))}
+}
+
+$encryptKey = Set-Key "encryptedpatvalue"
+$patkeyEncrypted = Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod' -Name 'PATTFVC'
+
+$patKeyDecrypted = Get-EncryptedData -data $patkeyEncrypted.SecretValueText -key $encryptKey
+
+$personalAccessToken=$patKeyDecrypted
+
+####END Cipher technique#######
+    
+    Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod'
+    
+   [string]$ServerName=(Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod' -Name 'SQLservername').SecretValueText   
+   [string]$UserName=(Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod' -Name 'username').SecretValueText
+    [string]$Password=(Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod' -Name 'sqlpassword').SecretValueText
+    $DbName=(Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod' -Name 'DBname').SecretValueText
+    
+    $Endusername=(Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod' -Name 'Euname').SecretValueText
+    $Enduserpassword=(Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod' -Name 'EUPassword').SecretValueText
+    $AZSubscription=(Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod' -Name 'AZSubscription').SecretValueText
+    $TenantId=(Get-AzureKeyVaultSecret -VaultName 'credscankeyvaultprod' -Name 'Azsubscriptiontenantid').SecretValueText
+
+
+    $Clonedir = "C:\Credscan\Repoclone"
+    $RgName="Credscan-RG"  
+    $RgLocation="Central US"
+    $storageaccountName="credscangitprodtest"
+    $ContainerName="credscancontainergit"
+
+
+  
+    #New-Item -ItemType directory -Path "C:\TFVCRepopathCSV"
+    $Armexist=Get-Module -Name AzureRM.* -ListAvailable
+    if(!($Armexist.Count -gt 0))
+    {
+        Install-Module -Name AzureRm -AllowClobber -Force -Verbose
+    }
+    $EU="$Endusername"
+    $EP= "$Enduserpassword"
+    $Subscription="$AZSubscription"
+    $AzureSubscriptionTenantId="$TenantId"
     $azureAccountName = $EU
     $azurePassword = ConvertTo-SecureString $EP -AsPlainText -Force
     $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
@@ -48,101 +117,68 @@ $count=1
 $connectionString = "Server=$ServerName;uid=$UserName; pwd=$Password;Database=$DbName;Integrated Security=False;"
 $connection = new-object system.data.SqlClient.SQLConnection($connectionString)
 Write-output "credential formed"
-
- function CloneTFVCRepo($prjName,$url,$PAT,$tfvcRepositoryPath,$tfvctoolpath)
-    {         
-        try
-        {
-            $cs_userName = "v-jamad@microsoft.com"            
-
-            New-Item -ItemType Directory -Path "$tfvcRepositoryPath\$prjName" -Force
-            cd $tfvcRepositoryPath\$prjName                    
-              #& "$tfvctoolpath\TFVCCloningExe\TFVCCloning.exe" $url $cs_userName $PAT $prjName gdbldsvc@microsoft.com $/$prjName $tfvcRepositoryPath\$prjName 
-            & "$tfvctoolpath\HelperTool\TFVCCloning.exe" $url $cs_userName $PAT $prjName v-jamad@microsoft.com $/$prjName $tfvcRepositoryPath\$prjName             
-        }
-        catch [System.Exception]
-        {
-            $message=$_ -replace "The running command stopped because the preference variable ""ErrorActionPreference"" or common parameter is set to Stop: ",""    
-            throw "$message"+"~Cloning"
-     
-        }        
-    }  
-
-    #New-Item -ItemType directory -Path "C:\TFVCRepopathCSV"
-   # New-Item -ItemType directory -Path "C:\TFVCRepopath"
-
-    $count=1
-
-    do
-   {
-
-    $connection.Open()
-    $command2 = $connection.CreateCommand() 
-    $command2.CommandText = "Exec usp_GetTFVCRepository" 
+do
+{
+    try
+    {
+      $connection.Open()
+     $command2 = $connection.CreateCommand() 
+    $command2.CommandText = "EXEC dbo.usp_GetCredscanGITRepos" 
     $dataAdapt = new-object System.Data.SqlClient.SqlDataAdapter $command2
     $dataS2 = New-Object System.Data.DataSet
    Write-Host $dataAdapt.Fill($dataS2)       
    Write-Host $dataS2.Tables.Count
-   $connection.Close()
-
-   if($dataS2.Tables.Count -gt 0 -And $dataS2.Tables[0].Rows.Count -gt 0)
     
+    if($dataS2.Tables.Count -gt 0 -And $dataS2.Tables[0].Rows.Count -gt 0)
+   
     {    
-   $count = 1
-                    $repopath1 =  $dataS2.Tables[0].Rows[0]["OrganizationURL"]
-                    $RepoName1 = $dataS2.Tables[0].Rows[0]["ProjectName"]
-                    $RepoId1 = $dataS2.Tables[0].Rows[0]["ProjID"]
+        $count = 1
+                           
+                    
+                    $accountname =  $dataS2.Tables[0].Rows[0]["AccountName"]
+                    $prjname = $dataS2.Tables[0].Rows[0]["ProjectName"]
+                    $repoName = $dataS2.Tables[0].Rows[0]["RepoName"]
+                    $RepoId = $dataS2.Tables[0].Rows[0]["ProjID"]
                 
             }
             else
             {
             $count = 0
             }
+           
+            if ($count -eq 1)
+            {
 
+       Write-output "entered into while block"
+        $connection.Close()
+        $dir = "C:\Repoclone\$RepoName"  
+        $toolPath ="C:\Credscan\tools\CredentialScanner.exe" 
+        $searcher="C:\Credscan\tools\Searchers\buildsearchers.xml"
+        $repoLogsOutput="C:\CSV\$RepoName"
 
+        $gitURL="https://$personalAccessToken@$accountName.visualstudio.com"
+        $gitFinalRepoPath = "$gitURL/$prjName/_git/$repoName".Trim().Replace(' ', '%20')    
 
-
-
-    CloneTFVCRepo -prjName $RepoName1 -url $repopath1 -PAT "levn24wy5wmx2gkyv5qo64smhu3yj4wkqyn572n3hmaxnxusjnjq" -tfvctoolpath "C:\TFVCCloneTool" -tfvcRepositoryPath "C:\TFVCRepopath"
-
-    ExecCredScan -projectName $RepoName1 -repoName $RepoName1 -searchersPath "C:\Credscan\tools\Searchers\buildsearchers.xml" -toolPath "C:\Credscan\tools\CredentialScanner.exe" -repositoryPath "C:\TFVCRepopath" -repoLogsOutput "C:\TFVCRepopathCSV"
-    
-
-     Set-AzureStorageBlobContent -Container $ContainerName -File "C:\TFVCRepopathCSV\$RepoName1-matches.csv" -Context $storeageaccount.Context -Force
-
-         $connection.Open()
-         $query1 = "update TFVC_CredScan set IsProcessed=1, IsAccessed = 0 where ProjID='$RepoId1' "
+        Write-Output "===Cloning repo $repopath===" 
+        git clone $gitFinalRepoPath $dir
+        Write-Output "$toolPath $dir $repoLogsOutput" 
+        & $toolPath -I "$dir" -S $searcher -O "$repoLogsOutput" -f csv -cp
+       
+        $connection.Open()
+        $query1 = "update CredScanGitRepos set IsProcessed=1, IsAccessed = 0 where ProjID='$RepoId' "
         $command1 = $connection.CreateCommand()
         $command1.CommandText = $query1
         $result = $command1.ExecuteReader()
          $connection.Close()
 
-    }
-    until ($count -eq 0)
-    
-    function ExecCredScan($projectName, $repoName, $searchersPath, $toolPath, $repoType, $accountName,$repositoryPath,$repoLogsOutput)
-    {     
-        try
-        {
-
-        #$repoLogsOutput = "$resultsPath\$accountName!!$projectName!!$repoName!!$repoType$Using:InstanceNo"     
-            $dir = "$repositoryPath\$repoName"       
-
-            $repoLogsOutput="C:\TFVCRepopathCSV\$projectName"
-            Write-Output "$repoLogsOutput"
-      
-            & $toolPath -I "$dir" -S "$searchersPath" -O "$repoLogsOutput" -f csv -cp
-        }
-        catch [System.Exception]
-        {
-            $message=$_ -replace "The running command stopped because the preference variable ""ErrorActionPreference"" or common parameter is set to Stop: ",""    
-            throw "$message"+"~Scanning"
-        }  
-    }
-
-   
-      #Set-AzureStorageBlobContent -Container $ContainerName -File "C:\TFVCRepopathCSV\$RepoName1-matches.csv" -Context $storeageaccount.Context -Force
+        Set-AzureStorageBlobContent -Container $ContainerName -File "$repoLogsOutput-matches.csv" -Context $storeageaccount.Context -Force
         Write-Output "====Scan Completed and status updated===="
-   
-
-#until ($count -eq 0)
+      
+        }
+    }
+    catch
+    {
+    throw $_.Exception
+    }
+}
+until ($count -eq 0)
